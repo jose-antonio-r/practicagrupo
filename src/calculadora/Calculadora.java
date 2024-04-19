5b09b73318580f82d2f41dbef2f717b4a8bbd9be
usuarioo30 <duranenriquezpastormiguel@gmail.com>
2024-04-19 09:27:36 +0200

    División

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,5 +1,44 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Menú calculadora");
+		System.out.println("----------------");
+		System.out.println("1. Dividir");
+		System.out.println("0. Salir");
+		System.out.print("Dime una opción: ");
+		int respuestaUsuario = Integer.parseInt(s.nextLine());
+
+		System.out.println();
+
+		do {
+
+			switch (respuestaUsuario) {
+			case 0:
+				System.out.println("Has salido al programa");
+				break;
+			case 1:
+				System.out.println("Dime un número: ");
+				int numero1 = Integer.parseInt(s.nextLine());
+
+				System.out.println("Dime otro número: ");
+				int numero2 = Integer.parseInt(s.nextLine());
+
+				System.out.println();
+
+				double resultado = numero1 / numero2;
+
+				System.out.printf("El resultado de %d / %d es: %.1f\n", numero1, numero2, resultado);
+
+				break;
+			}
+		} while (respuestaUsuario != 0);
+
+		System.out.println();
+	}
 }
