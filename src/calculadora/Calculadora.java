c63a854636066c849e51f49cff79ecbfc76f0072
usuario <estebanalfis@gmail.com>
2024-04-19 09:20:59 +0200

    Menú y multiplicación

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,5 +1,39 @@
 package calculadora;
 
-public class Calculadora {
+import java.util.Scanner;
 
+public class Calculadora {	
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		int opcion;
+		
+		do {
+		System.out.println("-----CALCULADORA----");
+		System.out.println("0. Salir");
+		System.out.println("1. Multiplicar");
+		System.out.println("--------------------");
+		System.out.print("Elige una opción: ");
+		opcion = Integer.parseInt(teclado.nextLine());
+		
+		
+		switch (opcion) {
+		case 1:
+			System.out.print("Introduce el primer valor: ");
+			double valor1 = Integer.parseInt(teclado.nextLine());
+			
+			System.out.print("Introduce el segundo valor: ");
+			double valor2 = Integer.parseInt(teclado.nextLine());
+			
+			System.out.printf("El resultado es: %.2f\n", valor1*valor2);
+		}
+		
+		} while (opcion != 0);
+		
+		
+		teclado.close();
+	}
+	
+	
 }
